f95381be0758dba06df8df63cb263cdb328cfc60
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-08-09 15:22:50 -0500

    generar zip

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from datetime import datetime
+import zipfile
+import random
 import xml.etree.ElementTree as ET
+from datetime import datetime
 from postgres import Postgres
+
+def zipdir(path, ziph):
+  # ziph is zipfile handle
+  for root, dirs, files in os.walk(path):
+    for file in files:
+      ziph.write(
+        os.path.join(root, file), 
+        os.path.relpath(os.path.join(root, file), 
+        os.path.join(path, '..'))
+      )
 
 # ids
 i = 0
 ids = ""
 while i < 5:
-  id = input("Ingrese ID a buscar: ")
-  ids = ids + "," + id
+  id = random.randint(1, 100)
+  ids = ids + "," + str(id)
   i = i + 1
 # db
 db = Postgres(url="postgres://root:123@127.0.0.1:5432/tickets?sslmode=disable")
@@ -37,3 +49,7 @@ for worker in workers:
         tmp.attrib["whastapp"] = worker.phone
   name = str(worker.id) + "_" + (worker.last_names + worker.names).replace(" ", "") + ".xml"
   tree.write(path + "/" + name, encoding="utf8", method="xml")
+# zip
+with zipfile.ZipFile("tmp/" + str(timestamp) + ".zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+  zipdir("tmp/" + str(timestamp) + "/", zipf)
+# email
